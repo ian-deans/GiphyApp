d62fddcdc2edfc1e2de8ea2de8138a3a2c21c56c
Ian Deans <ideans715@gmail.com>
2021-01-22 21:46:06 -0800

    change color scheme | clean up layout

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-console.log('Script Loaded.')
+console.log( 'Script Loaded.' );
 
 // Define Global Variables
 
-  // String used to build the queryString from
-var API_STRING = 'https://api.giphy.com/v1/gifs/search?api_key=aQEL7kkkxlgFj1n4VjjY3RVRFTETGYVP&limit=10&q='
+// String used to build the queryString from
+var API_STRING = 'https://api.giphy.com/v1/gifs/search?api_key=aQEL7kkkxlgFj1n4VjjY3RVRFTETGYVP&limit=10&q=';
 
-  // Reference to the html input for adding a search term
-var searchTermInput = $('.search-term-input');
+// Reference to the html input for adding a search term
+var searchTermInput = $( '.search-term-input' );
 
-  // Reference to the sidebar element that contains the search buttons
-var sideBar = $('.sidebar');
+// Reference to the sidebar element that contains the search buttons
+var sideBar = $( '.sidebar' );
 
-var gifContainer = $('.gif-images');
+var gifContainer = $( '.gif-images' );
 
 
 // Function used to assign event listeners to elements on initial page load
@@ -24,28 +24,28 @@ _appSetup();
 
 function _appSetup() {
   // Add event listener for form submission
-  $('#search-term-form').on( 'submit', addSearchButton );
+  $( '#search-term-form' ).on( 'submit', addSearchButton );
 
   // Add event listener for search button click
-  $('.search-btn').on( 'click', handleSearch );
+  $( '.search-btn' ).on( 'click', handleSearch );
 }
 
 // Function to take a search term and return an html button using that term
 function buildSearchButton( searchTerm ) {
-  var newButton = $( `<button> ${searchTerm} </button>` ); // Create the button element
+  var newButton = $( `<button> ${ searchTerm } </button>` ); // Create the button element
   newButton.addClass( 'search-btn' );                      // Add css class to button
   newButton.attr( 'data-term', searchTerm );               // Add data attribute to hold term
   newButton.on( 'click', handleSearch );                   // Attach event listener
   return newButton;                                        // return finished button
-};
+}
 
 // Add a new search button to the DOM
 function addSearchButton( event ) {
   event.preventDefault();   // Prevent the form from resubmitting the page
 
-  var newTerm = searchTermInput.val().trim()  // Collect the new search term
+  var newTerm = searchTermInput.val().trim();  // Collect the new search term
 
-  searchTermInput.val('');    // Clear the input field
+  searchTermInput.val( '' );    // Clear the input field
 
   var button = buildSearchButton( newTerm );  // Use function to create the element
   sideBar.append( button );   // Append the new button to the HTML document
@@ -58,11 +58,11 @@ function handleSearch() {
 
   var queryURL = API_STRING + term;  // Build the query using the API_STRING and term
 
-  fetch( queryURL, {method: 'GET'} )  // Send a 'GET' Ajax request to Giphy API
-    .then( function( result ) {       // Catch the response object
+  fetch( queryURL, { method: 'GET' } )  // Send a 'GET' Ajax request to Giphy API
+    .then( function ( result ) {       // Catch the response object
       return result.json();           // Parse the response into a javascript object
-    })
-    .then( handleApiResponse )
+    } )
+    .then( handleApiResponse );
 }
 
 
@@ -83,16 +83,16 @@ function buildImgs( urlData ) {
   var imgArray = [];    // Create an array outside of the loop to hold our data.
 
   for ( var i = 0; i < urlData.length; i++ ) {    // Define loop to iterate over urlData array
-    var container = $('<figure>');      // Create figure element that wraps the img
+    var container = $( '<figure>' );      // Create figure element that wraps the img
     container.addClass( 'gif-image-container' );    // Add css class to element
 
-    var img = $('<img>');   // Create img element
-    img.attr( 'src', urlData[i].still );              // Add the src attribute
+    var img = $( '<img>' );   // Create img element
+    img.attr( 'src', urlData[ i ].still );              // Add the src attribute
     img.attr( 'data-state', 'still' );                // Add the data-state attribute
-    img.attr( 'data-still', urlData[i].still );       // Add the still url as a data variable
-    img.attr( 'data-animate', urlData[i].animate );   // Add the animated url as a data variable
+    img.attr( 'data-still', urlData[ i ].still );       // Add the still url as a data variable
+    img.attr( 'data-animate', urlData[ i ].animate );   // Add the animated url as a data variable
 
-    container.on( 'click', handleClickGif)    // Add the click event listener to the figure element
+    container.on( 'click', handleClickGif );    // Add the click event listener to the figure element
 
     container.append( img );                  // Put the img inside of the figure
     imgArray.push( container );               // Push the figure which contains the img into our array
@@ -106,7 +106,7 @@ function extractUrls( collection ) {
   var gifData = [];   // Array to hold the objects containing the URLs
 
   for ( var i = 0; i < collection.length; i++ ) {   // Define our loop to iterate over the collection
-    var item = collection[i].images;    // Abstract the data into a variable for less typing
+    var item = collection[ i ].images;    // Abstract the data into a variable for less typing
 
     var urls = {                        // Create a temp object to hold the urls for this gif
       still: item.downsized_still.url,  // Save the still url under the key 'still'
@@ -123,7 +123,7 @@ function handleClickGif( event ) {
   var gif = event.target;     // Grab the target of the click event ( the img element )
 
   if ( gif.dataset.state === 'still' ) {    // If the state of the gif is still
-    gif.dataset.state = 'animated'          // Set its state to 'animated'
+    gif.dataset.state = 'animated';          // Set its state to 'animated'
     gif.src = gif.dataset.animate;          // Switch the src of the img to the animated url
   } else {
     gif.dataset.state = 'still';            // Otherwise set it's state to still
